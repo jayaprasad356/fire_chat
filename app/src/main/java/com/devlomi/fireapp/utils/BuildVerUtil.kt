004ea0ebf288b05ba925fc284eee8f14d006333d
jayaprasad356 <jayaprasad356@gmail.com>
2022-12-15 09:41:47 +0530

    Initial commit

---
+++ app/src/main/java/com/devlomi/fireapp/utils/BuildVerUtil.kt
@@ -0,0 +1,19 @@
+package com.devlomi.fireapp.utils
+
+import android.os.Build
+
+object BuildVerUtil {
+    @JvmStatic
+    fun isOreoOrAbove(): Boolean {
+        return Build.VERSION.SDK_INT >= 26
+    }
+
+    @JvmStatic
+    fun isApi29OrAbove(): Boolean {
+        return Build.VERSION.SDK_INT >= 29
+    }
+
+    @JvmStatic
+    fun isApi31OrAbove(): Boolean = Build.VERSION.SDK_INT >= 31
+
+}
